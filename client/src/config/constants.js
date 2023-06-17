bdb3ffe4b8a95a6c4e74fb8d2bb0a973e80d70dd
soumitradev <soumitra.shewale.dev@gmail.com>
2023-06-18 00:36:00 +0530

    fix: fix typo in google auth callback URL

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,6 @@
 const prod = {
   urls: {
-    googleAuth: "https://crux-bpc.com/chrono/api/auth/google",
+    googleAuth: "https://crux-bphc.com/chrono/api/auth/google",
     adminLogin: "https://chrono-dashboard.herokuapp.com/",
   },
 }
